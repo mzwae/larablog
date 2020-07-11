aa55406473dc7194bc9c0e0bdbaa4040f02d609c
mzwae <mrmonjaie@gmail.com>
2020-07-11 23:51:04 +0100

    Update homepage link and logo link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,11 +27,11 @@ Released   : 20140225
     <div id="header-wrapper">
         <div id="header" class="container">
             <div id="logo">
-                <h1><a href="#">SimpleWork</a></h1>
+                <h1><a href="/">SimpleWork</a></h1>
             </div>
             <div id="menu">
                 <ul>
-                    <li class="current_page_item"><a href="#" accesskey="1" title="">Homepage</a></li>
+                    <li class="current_page_item"><a href="/" accesskey="1" title="">Homepage</a></li>
                     <li><a href="#" accesskey="2" title="">Our Clients</a></li>
                     <li><a href="/about" accesskey="3" title="">About Us</a></li>
                     <li><a href="#" accesskey="4" title="">Careers</a></li>
