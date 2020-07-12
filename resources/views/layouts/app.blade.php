28e2cddced68f0bd1e295204a7d708ebcb42f56c
mzwae <mrmonjaie@gmail.com>
2020-07-12 23:44:20 +0100

    Add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,6 +42,9 @@ Released   : 20140225
 
     @yield('header')
     @yield('content')
+    <div id="copyright" class="container">
+        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+    </div>
 </body>
 </html>
 
