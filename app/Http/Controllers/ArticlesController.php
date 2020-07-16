31540145e250014efa5a533aaffe3c8778394e15
mzwae <mrmonjaie@gmail.com>
2020-07-16 22:50:51 +0100

    Add create article ctrl method

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Article;
-use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
+
+    /**
+     *  Show a single article
+     */
     public function show($id)
     {
         $article = Article::find($id);
@@ -14,11 +17,58 @@ class ArticlesController extends Controller
         return view('articles.show', ['article' => $article]);
     }
 
+    /**
+     *  Render a list of articles
+     */
 
     public function index()
     {
         $articles = Article::latest()->get();
 
         return view('articles.index', ['articles' => $articles]);
+    }
+
+    /**
+     * Shows a view to create a new article
+     */
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    /**
+     * Persist the newly created article
+     */
+
+    public function store()
+    {
+
+    }
+
+    /**
+     * Shows a view to edit an existing article
+     */
+
+    public function edit()
+    {
+
+    }
+
+    /**
+     * Persist the edited article
+     */
+
+    public function update()
+    {
+
+    }
+
+    /**
+     * Delete an article
+     */
+    public function destroy()
+    {
+
     }
 }
