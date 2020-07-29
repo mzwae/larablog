0aaf837c5e29ed14fa6ed25030921b6a91810479
mzwae <mrmonjaie@gmail.com>
2020-07-29 22:29:27 +0100

    Add create article nav link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,15 +29,14 @@ Released   : 20140225
     <div id="header-wrapper">
         <div id="header" class="container">
             <div id="logo">
-                <h1><a href="/">SimpleWork</a></h1>
+                <h1><a href="/">Brand</a></h1>
             </div>
             <div id="menu">
                 <ul>
                     <li class="{{ Request::is('/') ? 'current_page_item' : ''}}"><a href="/" accesskey="1" title="">Homepage</a></li>
-                    <li class="{{ Request::is('clients') ? 'current_page_item' : ''}}"><a href="#" accesskey="2" title="">Our Clients</a></li>
                     <li class="{{ Request::is('about') ? 'current_page_item' : ''}}"><a href="/about" accesskey="3" title="">About Us</a></li>
                     <li class="{{ Request::is('articles') ? 'current_page_item' : ''}}"><a href="/articles" accesskey="4" title="">Articles</a></li>
-                    <li class="{{ Request::is('contact') ? 'current_page_item' : ''}}"><a href="#" accesskey="5" title="">Contact Us</a></li>
+                    <li class="{{ Request::is('create') ? 'current_page_item' : ''}}"><a href="{{route('articles.create')}}" accesskey="5" title="">Create Article</a></li>
                 </ul>
             </div>
         </div>
