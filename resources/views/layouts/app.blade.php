ae0c1945559c0f75ce0804b4be72cae3dca2cab0
mzwae <mrmonjaie@gmail.com>
2020-07-30 23:43:51 +0100

    Move html head to separate file to refactor code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,17 +12,7 @@ Released   : 20140225
 -->
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>{{ env('APP_NAME') }}</title>
-
-
-<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-<link href="/default.css" rel="stylesheet" type="text/css" media="all" />
-<link href="/fonts.css" rel="stylesheet" type="text/css" media="all" />
-
-@yield('head')
-
+    @include('layouts.head')
 </head>
 <body>
     @include('layouts.navbar')
