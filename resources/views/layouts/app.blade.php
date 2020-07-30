3cdf5cead35b870a260713ad3f224b24be0dc7cf
mzwae <mrmonjaie@gmail.com>
2020-07-30 23:44:55 +0100

    Reorganize code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,15 +11,15 @@ Released   : 20140225
 
 -->
 <html>
-<head>
-    @include('layouts.head')
-</head>
-<body>
-    @include('layouts.navbar')
-    @yield('header')
-    @yield('content')
-    <div id="copyright" class="container">
-        <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
-    </div>
-</body>
+    <head>
+        @include('layouts.head')
+    </head>
+    <body>
+        @include('layouts.navbar')
+        @yield('header')
+        @yield('content')
+        <div id="copyright" class="container">
+            <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+        </div>
+    </body>
 </html>
