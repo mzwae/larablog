33c137be8d8f184b1b03041fe32558a222ed02c2
mzwae <mrmonjaie@gmail.com>
2020-07-31 21:51:27 +0100

    Reorganize code

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,22 +2,26 @@
 
 @section('content')
 
-<div id="wrapper">
-	<div id="page" class="container">
-		<div id="content">
-			<div class="title">
-				<h2>{{ $article->title }}</h2>
-            <p><img src="/images/banner.jpg" alt="" class="image image-full" /> </p>
-            {{ $article->body }}
+    <div id="wrapper">
+        <div id="page" class="container">
+            <div id="content">
+                <div class="title">
 
-            <p class="mt-4">
-                @foreach($article->tags as $tag)
-                    <a href="{{ route('articles.index', ['tag' => $tag->name])}}">{{ $tag->name }}</a>
-                @endforeach
-            </p>
+                    <h2>{{ $article->title }}</h2>
+                    <img src="/images/banner.jpg" alt="" class="image image-full"/>
+                    <p>
+                        {{ $article->body }}
+                    </p>
+                    <p class="mt-4">
+                        @foreach($article->tags as $tag)
+                            <a href="{{ route('articles.index', ['tag' => $tag->name]) }}">{{ $tag->name }}</a>
+                        @endforeach
+                    </p>
 
-        <a href="{{route('articles.edit', ['article' => $article->id])}}" class="btn btn-info">Edit</a>
-		</div>
-	</div>
-</div>
+                    <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-info">Edit</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
