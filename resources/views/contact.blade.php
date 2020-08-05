b9bd83836714b19d6563c2bd6df39be86bb11951
mzwae <mrmonjaie@gmail.com>
2020-08-05 22:19:40 +0100

    Add contact page view

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+
+
+<div class="text-center mt-5 border border-5 border-success w-50 p-5 mx-auto">
+<form method="POST" action="{{route('contact')}}">
+    @csrf
+        <div class="form-group">
+            <label for="email">Email Address</label>
+            <input type="email" class="form-control" name="email" id="email" placeholder="Type your email here">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Email Me</button>
+
+    </form>
+
+</div>
+
+</div>
+
+
+@endsection
