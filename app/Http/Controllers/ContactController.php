a5d2f597fccdfcd32b3169b9f12a3aaa6c659a91
mzwae <mrmonjaie@gmail.com>
2020-08-07 23:30:13 +0100

    Replace raw email with markdown

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -17,12 +18,8 @@ class ContactController extends Controller
     {
         request()->validate(['email' => 'required|email']);
 
-        Mail::raw('This is just to confirm your email is valid', function ($message) {
-            $message->from('john@johndoe.com', 'John Doe')
-            ->to(request('email'))
-            ->subject('Hello From LaraBlog');
+        Mail::to(request('email'))->send(new Contact());
 
-        });
 
         return redirect(route('contact'))->with('message', 'Thank You. Your email has been sent!');
     }
