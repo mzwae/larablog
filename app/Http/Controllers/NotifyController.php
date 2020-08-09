e0716c6456c181196da94d66173d34e0ab62f64c
mzwae <mrmonjaie@gmail.com>
2020-08-09 21:42:11 +0100

    Add route handler functions

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class NotifyController extends Controller
 {
-    //
+    public function show()
+    {
+        return view('notify');
+    }
+
+
+    public function store()
+    {
+        request()->validate(['email' => 'required|email']);
+
+
+        return redirect(route('notify'))->with('message', 'Thank You. Your email has been sent!');
+    }
 }
