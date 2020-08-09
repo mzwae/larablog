49291874dbdf97f6adfd0a249eb6211d6c27acf6
mzwae <mrmonjaie@gmail.com>
2020-08-09 23:44:54 +0100

    Add notify user fascade

--- app/Http/Controllers/NotifyController.php
+++ app/Http/Controllers/NotifyController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Notifications\EmailReceived;
+use Illuminate\Support\Facades\Notification;
 
 class NotifyController extends Controller
 {
@@ -16,6 +18,7 @@ class NotifyController extends Controller
     {
         request()->validate(['email' => 'required|email']);
 
+        Notification::send(request()->user(), new EmailReceived());
 
         return redirect(route('notify'))->with('message', 'Thank You. Your email has been sent!');
     }
