a7f7973faa6fc346042230cc832509e5149749aa
mzwae <mrmonjaie@gmail.com>
2020-08-10 22:01:44 +0100

    Remove user email input field

--- resources/views/notify.blade.php
+++ resources/views/notify.blade.php
@@ -13,14 +13,6 @@
     <div class="text-center mt-5 border border-5 border-success w-50 p-5 mx-auto">
         <form method="POST" action="{{route('notify.post')}}">
             @csrf
-            <div class="form-group">
-                <label for="email">Email Address</label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="Type your email here">
-
-                @error('email')
-                    <div class="text-danger">{{$message}}</div>
-                @enderror
-            </div>
 
             <button type="submit" class="btn btn-primary">Notify Me</button>
 
