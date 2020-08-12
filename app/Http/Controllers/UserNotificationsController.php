8b1b8fe61f514df893a44790a33982aff8569298
mzwae <mrmonjaie@gmail.com>
2020-08-12 22:58:54 +0100

    Attach notifications to view

--- app/Http/Controllers/UserNotificationsController.php
+++ app/Http/Controllers/UserNotificationsController.php
@@ -8,6 +8,8 @@ class UserNotificationsController extends Controller
 {
     public function show()
     {
-        return view('notifications.show');
+        return view('notifications.show', [
+            'notifications' => auth()->user()->notifications
+        ]);
     }
 }
