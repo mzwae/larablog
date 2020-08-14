fc42b69a418429bb6e6b640c0d093cad036d58d2
mzwae <mrmonjaie@gmail.com>
2020-08-14 23:08:18 +0100

    Add nexmo message content

--- app/Notifications/EmailReceived.php
+++ app/Notifications/EmailReceived.php
@@ -3,9 +3,9 @@
 namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Messages\NexmoMessage;
 
 class EmailReceived extends Notification
 {
@@ -43,12 +43,24 @@ class EmailReceived extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->subject('Your LaraBlog Payment Was Received')
-                    ->greeting("What's Up?")
-                    ->line('The introduction to the notification.')
-                    ->line('Id aute sint ullamco pariatur cillum elit do labore laboris velit et laborum magna ullamco..')
-                    ->action('Sign Up', url('/'))
-                    ->line('Thanks Buddy!');
+            ->subject('Your LaraBlog Payment Was Received')
+            ->greeting("What's Up?")
+            ->line('The introduction to the notification.')
+            ->line('Id aute sint ullamco pariatur cillum elit do labore laboris velit et laborum magna ullamco..')
+            ->action('Sign Up', url('/'))
+            ->line('Thanks Buddy!');
+    }
+
+    /**
+     * Get the Nexmo / SMS representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return NexmoMessage
+     */
+    public function toNexmo($notifiable)
+    {
+        return (new NexmoMessage)
+            ->content('Your LaraBlog account has been set up successfully!');
     }
 
     /**
@@ -60,7 +72,7 @@ class EmailReceived extends Notification
     public function toArray($notifiable)
     {
         return [
-            'amount' => $this->amount
+            'amount' => $this->amount,
         ];
     }
 }
