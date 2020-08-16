d8d2f24863e2cf5802813916ba7ecc7326881fff
mzwae <mrmonjaie@gmail.com>
2020-08-16 22:38:00 +0100

    Add bootstrap css classes

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -1,50 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <div id="wrapper">
-        <div id="page" class="container">
-            <h1 class="heading has-text-weight-bold is-size-4">Edit Article</h1>
+<div id="wrapper">
+    <div id="page" class="container">
+        <h1 class="heading has-text-weight-bold is-size-4">Edit Article</h1>
         <form action="/articles/{{ $article->id }}" method="post">
-                @csrf
-                @method('put')
-                <div class="field">
-                    <label for="title" class="lable">
-                        Title
-                    </label>
+            @csrf
+            @method('put')
+            <div class="form-group">
+                <label for="title" class="lable">
+                    Title
+                </label>
 
-                    <div class="control">
-                    <input type="text" class="input" name="title" id="title" value="{{ $article->title }}">
-                    </div>
-                </div>
+                <input type="text" class="form-control" name="title" id="title" value="{{ $article->title }}">
+            </div>
 
-                <div class="field">
-                    <label for="excerpt" class="label">
-                        Excerpt
-                    </label>
+            <div class="form-group">
+                <label for="excerpt" class="label">
+                    Excerpt
+                </label>
 
-                    <div class="control">
-                    <textarea name="excerpt" id="excerpt" cols="30" rows="10" class="textarea" >{{ $article->excerpt }}</textarea>
-                    </div>
-                </div>
+                <textarea name="excerpt" id="excerpt" cols="30" rows="10" class="form-control">{{ $article->excerpt }}</textarea>
+            </div>
 
-                <div class="field">
-                    <label for="body" class="label">
-                        Body
-                    </label>
+            <div class="form-group">
+                <label for="body" class="label">
+                    Body
+                </label>
 
-                    <div class="control">
-                        <textarea name="body" id="excerpt" cols="30" rows="10" class="textarea">{{ $article->body }}</textarea>
-                    </div>
-                </div>
+                <textarea name="body" id="excerpt" cols="30" rows="10" class="form-control">{{ $article->body }}</textarea>
+            </div>
 
-                <div class="field is-grouped">
+            <button class="btn btn-primary" type="submit">Submit</button>
 
-                    <div class="control">
-                        <button class="button is-link" type="submit">Submit</button>
-                    </div>
-
-                </div>
-            </form>
-        </div>
     </div>
+    </form>
+</div>
+</div>
 @endsection
