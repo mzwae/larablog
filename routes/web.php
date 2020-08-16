d4c53e071c30b3140830064b7d75925c9d6bb8d8
mzwae <mrmonjaie@gmail.com>
2020-08-16 22:25:12 +0100

    Restrict access to create articles to auth users only

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ Route::get('articles', 'ArticlesController@index')->name('articles.index');
 
 Route::post('articles', 'ArticlesController@store')->name('articles.post');
 
-Route::get('articles/create', 'ArticlesController@create')->name('articles.create');
+Route::get('articles/create', 'ArticlesController@create')->name('articles.create')->middleware('auth');
 
 Route::get('articles/{article}', 'ArticlesController@show')->name('articles.show');
 
