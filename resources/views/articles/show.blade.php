94f1d6f7e9f3e327fd452a4f3de6aab1e5056e99
mzwae <mrmonjaie@gmail.com>
2020-08-16 22:40:34 +0100

    Restrict edit btn view for auth users only

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,26 +2,28 @@
 
 @section('content')
 
-    <div id="wrapper">
-        <div id="page" class="container">
-            <div id="content">
-                <div class="title">
+<div id="wrapper">
+    <div id="page" class="container">
+        <div id="content">
+            <div class="title">
 
-                    <h2>{{ $article->title }}</h2>
-                    <img src="/images/banner.jpg" alt="" class="image image-full"/>
-                    <p>
-                        {{ $article->body }}
-                    </p>
-                    <p class="mt-4">
-                        @foreach($article->tags as $tag)
-                            <a href="{{ route('articles.index', ['tag' => $tag->name]) }}">{{ $tag->name }}</a>
-                        @endforeach
-                    </p>
+                <h2>{{ $article->title }}</h2>
+                <img src="/images/banner.jpg" alt="" class="image image-full" />
+                <p>
+                    {{ $article->body }}
+                </p>
+                <p class="mt-4">
+                    @foreach($article->tags as $tag)
+                    <a href="{{ route('articles.index', ['tag' => $tag->name]) }}">{{ $tag->name }}</a>
+                    @endforeach
+                </p>
 
-                    <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-info">Edit</a>
-                </div>
+                @auth
+                <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-info">Edit</a>
+                @endauth
             </div>
         </div>
     </div>
+</div>
 
 @endsection
