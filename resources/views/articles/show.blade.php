0bbc6f777b6c9d556ccd915fbfaf4e11b2e153f7
mzwae <mrmonjaie@gmail.com>
2020-08-17 23:02:31 +0100

    Fix bootstrap container view

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -4,26 +4,22 @@
 
 <div id="wrapper">
     <div id="page" class="container">
-        <div id="content">
-            <div class="title">
 
-                <h2>{{ $article->title }}</h2>
-                <small>Written by {{ $article->author->name }} on {{$article->created_at}}</small>
-                <img src="/images/banner.jpg" alt="" class="image image-full" />
-                <p>
-                    {{ $article->body }}
-                </p>
-                <p class="mt-4">
-                    @foreach($article->tags as $tag)
-                    <a href="{{ route('articles.index', ['tag' => $tag->name]) }}">{{ $tag->name }}</a>
-                    @endforeach
-                </p>
+        <h2>{{ $article->title }}</h2>
+        <small>Written by {{ $article->author->name }} on {{$article->created_at}}</small>
+        <img src="/images/banner.jpg" alt="" class="image image-full" />
+        <p>
+            {{ $article->body }}
+        </p>
+        <p class="mt-4">
+            @foreach($article->tags as $tag)
+            <a href="{{ route('articles.index', ['tag' => $tag->name]) }}">{{ $tag->name }}</a>
+            @endforeach
+        </p>
 
-                @auth
-                <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-info">Edit</a>
-                @endauth
-            </div>
-        </div>
+        @auth
+        <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-info">Edit</a>
+        @endauth
     </div>
 </div>
 
