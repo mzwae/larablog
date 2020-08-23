38de63d07083058fcaa04d6a3e30e150773cfe2c
mzwae <mrmonjaie@gmail.com>
2020-08-23 22:23:27 +0100

    Update delete modal design

--- resources/views/layouts/deletemodal.blade.php
+++ resources/views/layouts/deletemodal.blade.php
@@ -1,25 +1,26 @@
             <!-- Delete Reply Button Modal HTML -->
             <div id="deleteModal" class="modal fade delete-modal">
                 <div class="modal-dialog modal-confirm">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <div class="icon-box">
-                        <i class="material-icons">&#xE5CD;</i>
-                      </div>
-                      <h4 class="modal-title">Are you sure?</h4>
-                              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h4 class="modal-title">
+                              <i class="fas fa-trash"> Are you sure?</i>
+                            </h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                        </div>
+                        <div class="modal-body">
+                            <p>Do you really want to delete this? This process cannot be undone.</p>
+                        </div>
+                        <div class="modal-footer">
+                            <form action="{{route('articles.delete', ['article' => $article->id])}}" method="POST">
+                                <input value="Delete" type="submit" class="btn btn-danger">
+                                @method('DELETE')
+                                @CSRF
+                            </form>
+                            <a type="button" class="btn btn-info" data-dismiss="modal">
+                              <i class="fas fa-window-close"> Cancel</i>
+                            </a>
+                        </div>
                     </div>
-                    <div class="modal-body">
-                      <p>Do you really want to delete this? This process cannot be undone.</p>
-                    </div>
-                    <div class="modal-footer">
-                      <form action="{{route('articles.delete', ['article' => $article->id])}}" method="POST">
-                        <input value="Delete" type="submit" class="btn btn-danger">
-                        @method('DELETE')
-                        @CSRF
-                      </form>
-                      <a type="button" class="btn btn-info" data-dismiss="modal">Cancel</a>
-                    </div>
-                  </div>
                 </div>
-              </div>
+            </div>
