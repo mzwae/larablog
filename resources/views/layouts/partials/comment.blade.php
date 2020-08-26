546624409748bd84e91053b34722bb941a5b830e
mzwae <mrmonjaie@gmail.com>
2020-08-26 23:28:49 +0100

    Add dynamic data

--- resources/views/layouts/partials/comment.blade.php
+++ resources/views/layouts/partials/comment.blade.php
@@ -9,27 +9,19 @@
             </div>
             <div class="comment-widgets">
                 <!-- Comment Row -->
-                <div class="d-flex flex-row comment-row m-t-0">
-                    <div class="p-2"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1574583336/AAA/4.jpg" alt="user" width="50" class="rounded-circle"></div>
-                    <div class="comment-text w-100">
-                        <h6 class="font-medium">James Thomas</h6> <span class="m-b-15 d-block">This is awesome website. I would love to comeback again. </span>
-                        <div class="comment-footer"> <span class="text-muted float-right">April 14, 2019</span></div>
-                    </div>
-                </div> <!-- Comment Row -->
-                <div class="d-flex flex-row comment-row">
-                    <div class="p-2"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1574583319/AAA/3.jpg" alt="user" width="50" class="rounded-circle"></div>
-                    <div class="comment-text active w-100">
-                        <h6 class="font-medium">Michael Hussey</h6> <span class="m-b-15 d-block">Thanks bbbootstrap.com for providing such useful snippets. </span>
-                        <div class="comment-footer"> <span class="text-muted float-right">May 10, 2019</span> </div>
-                    </div>
-                </div> <!-- Comment Row -->
-                <div class="d-flex flex-row comment-row">
-                    <div class="p-2"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1574583246/AAA/2.jpg" alt="user" width="50" class="rounded-circle"></div>
-                    <div class="comment-text w-100">
-                        <h6 class="font-medium">Johnathan Doeting</h6> <span class="m-b-15 d-block">Great industry leaders are not the real heroes of stock market. </span>
-                        <div class="comment-footer"> <span class="text-muted float-right">August 1, 2019</span> </div>
+            @foreach ($comments as $comment)
+            <div class="d-flex flex-row comment-row m-t-0">
+                <div class="p-2"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1574583336/AAA/4.jpg" alt="user" width="50" class="rounded-circle"></div>
+                <div class="comment-text w-100">
+                <h6 class="font-medium">{{ $user->find($comment->commented_id)->name }}</h6>
+                <span class="m-b-15 d-block">{{ $comment->comment }}}</span>
+                    <div class="comment-footer">
+                    <span class="text-muted float-right">{{ $comment->created_at }}</span>
                     </div>
                 </div>
+            </div>
+            @endforeach
+
             </div> <!-- Card -->
         </div>
     </div>
