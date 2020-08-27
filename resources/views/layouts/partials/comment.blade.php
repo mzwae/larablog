1da939604cbb1fce7fe5ee504f14dda9b2b934a0
mzwae <mrmonjaie@gmail.com>
2020-08-27 22:55:29 +0100

    Add write comment form

--- resources/views/layouts/partials/comment.blade.php
+++ resources/views/layouts/partials/comment.blade.php
@@ -1,26 +1,40 @@
 <hr>
 
+<div class="row">
+    <div class="col-lg-6">
+
+    </div>
+</div>
 
 <div class="row d-flex justify-content-center mt-100 mb-100">
     <div class="col-lg-6">
+        <form action="" class="mb-3">
+            <div class="form-group">
+                <div class="form-group">
+                    <label for="comment"></label>
+                    <textarea class="form-control" name="comment" id="" rows="3" placeholder="Add your comment here..."></textarea>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
         <div class="card">
             <div class="card-body text-center">
                 <h4 class="card-title">Latest Comments</h4>
             </div>
             <div class="comment-widgets">
                 <!-- Comment Row -->
-            @foreach ($comments as $comment)
-            <div class="d-flex flex-row comment-row m-t-0">
-                <div class="p-2"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1574583336/AAA/4.jpg" alt="user" width="50" class="rounded-circle"></div>
-                <div class="comment-text w-100">
-                <h6 class="font-medium">{{ $user->find($comment->commented_id)->name }}</h6>
-                <span class="m-b-15 d-block">{{ $comment->comment }}}</span>
-                    <div class="comment-footer">
-                    <span class="text-muted float-right">{{ $comment->created_at }}</span>
+                @foreach ($comments as $comment)
+                <div class="d-flex flex-row comment-row m-t-0">
+                    <div class="p-2"><img src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1574583336/AAA/4.jpg" alt="user" width="50" class="rounded-circle"></div>
+                    <div class="comment-text w-100">
+                        <h6 class="font-medium">{{ $user->find($comment->commented_id)->name }}</h6>
+                        <span class="m-b-15 d-block">{{ $comment->comment }}}</span>
+                        <div class="comment-footer">
+                            <span class="text-muted float-right">{{ $comment->created_at }}</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            @endforeach
+                @endforeach
 
             </div> <!-- Card -->
         </div>
