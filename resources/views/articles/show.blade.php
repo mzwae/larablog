a4f17d70b1761853ff004826112af166e0a608e0
mzwae <mrmonjaie@gmail.com>
2020-09-05 22:33:45 +0100

    Display warning msg to unauthorized editing

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -12,7 +12,17 @@
                 <span aria-hidden="true">&times;</span>
                 <span class="sr-only">Close</span>
             </button>
-            <strong>Success!</strong> {{ session('message') }}
+             <strong>Success: </strong>{{ session('message') }}
+        </div>
+        @endif
+
+        @if(session('warning'))
+        <div class="alert alert-warning alert-dismissible fade show" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                <span class="sr-only">Close</span>
+            </button>
+             <strong>Warning: </strong>{{ session('warning') }}
         </div>
         @endif
 
