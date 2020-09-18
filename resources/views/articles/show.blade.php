4729ee2a6e1661914ea160327758bbc80aec84ed
mzwae <mrmonjaie@gmail.com>
2020-09-18 23:04:42 +0100

    Update single article view design

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -4,72 +4,77 @@
 
 <div id="wrapper">
     <div id="page" class="container">
+        <div class="row">
+            <div class="col-12">
+                {{-- Notify user about the newly added comment --}}
+                @if(session('message'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        <span class="sr-only">Close</span>
+                    </button>
+                    <strong>Success: </strong>{{ session('message') }}
+                </div>
+                @endif
 
-        {{-- Notify user about the newly added comment --}}
-        @if(session('message'))
-        <div class="alert alert-success alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                <span class="sr-only">Close</span>
-            </button>
-            <strong>Success: </strong>{{ session('message') }}
+
+
+                {{-- Notify user editing articles only for authors only --}}
+                @if(session('warning'))
+                <div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        <span class="sr-only">Close</span>
+                    </button>
+                    <strong>Warning: </strong>{{ session('warning') }}
+                </div>
+                @endif
+                {{-- notifications end --}}
+
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <h2>{{ $article->title }}</h2>
+                    <p class="pr-5">Written by {{ $article->author->name }} on {{ $article->created_at }} <span class="badge badge-success badge-pill">{{ $article->totalCommentsCount() }} comments</span></p>
+                </div>
+            </div>
+            {{-- Article Rating stars --}}
+            @for($i = round($article->averageRate()); $i > 0; $i--)
+            <span class="fa fa-star text-success"></span>
+            @endfor
+            @for($i = 5-round($article->averageRate()); $i > 0; $i--)
+            <span class="fa fa-star"></span>
+            @endfor
+            {{-- Article rating stars end --}}
+
+            <img src="/images/banner.jpg" alt="" class="img-thumbnail d-block mt-5 mb-5" />
+            <p>
+                {!! $article->body !!}
+            </p>
+            <p class="mt-4">
+                @foreach($article->tags as $tag)
+                <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"><span class="badge badge-primary">{{ $tag->name }}</span></a>
+                @endforeach
+            </p>
+
+            <hr>
+
+            @auth
+            @if(Auth::user()->id === $article->author->id)
+            <div class="m-5">
+                <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-info">
+                    <i class="fas fa-edit"> Edit</i>
+                </a>
+                <a data-toggle="modal" data-target="#deleteModal" title="Delete Article" type="button" class="btn btn-danger pull-right">
+                    <i class="fas fa-trash-alt"> Delete</i>
+                </a>
+            </div>
+            @endif
+            @endauth
+
+            @include('layouts.partials.comment')
+
         </div>
-        @endif
-
-
-
-        {{-- Notify user editing articles only for authors only --}}
-        @if(session('warning'))
-        <div class="alert alert-warning alert-dismissible fade show" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                <span class="sr-only">Close</span>
-            </button>
-            <strong>Warning: </strong>{{ session('warning') }}
-        </div>
-        @endif
-        {{-- notifications end --}}
-
-
-
-        <h2>{{ $article->title }}</h2>
-        <small class="pr-5">Written by {{ $article->author->name }} on {{ $article->created_at }} <span class="badge badge-success badge-pill">{{ $article->totalCommentsCount() }} comments</span></small>
-
-        {{-- Article Rating stars --}}
-        @for($i = round($article->averageRate()); $i > 0; $i--)
-        <span class="fa fa-star text-success"></span>
-        @endfor
-        @for($i = 5-round($article->averageRate()); $i > 0; $i--)
-        <span class="fa fa-star"></span>
-        @endfor
-        {{-- Article rating stars end --}}
-
-        <img src="/images/banner.jpg" alt="" class="img-thumbnail d-block mt-5 mb-5" />
-        <p>
-            {!! $article->body !!}
-        </p>
-        <p class="mt-4">
-            @foreach($article->tags as $tag)
-            <a href="{{ route('articles.index', ['tag' => $tag->name]) }}"><span class="badge badge-primary">{{ $tag->name }}</span></a>
-            @endforeach
-        </p>
-
-        <hr>
-
-        @auth
-        @if(Auth::user()->id === $article->author->id)
-        <div class="m-5">
-            <a href="{{ route('articles.edit', ['article' => $article->id]) }}" class="btn btn-info">
-                <i class="fas fa-edit"> Edit</i>
-            </a>
-            <a data-toggle="modal" data-target="#deleteModal" title="Delete Article" type="button" class="btn btn-danger pull-right">
-                <i class="fas fa-trash-alt"> Delete</i>
-            </a>
-        </div>
-        @endif
-        @endauth
-
-        @include('layouts.partials.comment')
 
     </div>
 </div>
