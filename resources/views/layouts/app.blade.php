0c5e1c03e15b1baf2633a2489a88116085011eb7
mzwae <mrmonjaie@gmail.com>
2020-09-25 20:23:39 +0100

    Include footer view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,19 +9,70 @@
         })
 
     </script>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        html,
+        body {
+            height: 100%;
+        }
+
+        #wrap {
+            min-height: 100%;
+        }
+
+        #main {
+            overflow: auto;
+            padding-bottom: 180px;
+            /* must be same height as the footer */
+        }
+
+        #footer {
+            position: relative;
+            margin-top: -180px;
+            /* negative value of footer height */
+            height: 180px;
+            clear: both;
+            background-color: grey;
+        }
+
+
+        /* Opera Fix thanks to Maleika (Kohoutec) */
+
+        body:before {
+            content: "";
+            height: 100%;
+            float: left;
+            width: 0;
+            margin-top: -32767px;
+            /* thank you Erik J - negate effect of float*/
+        }
+
+    </style>
+
+
 </head>
 
 
 <body>
-   <div class="container">
-    @include('layouts.navbar')
-    @yield('header')
-    @yield('content')
-   </div>
+    <div class="container" id="wrap">
 
-    <div id="copyright" class="container">
-        <p>&copy; MZ Apps. All rights reserved.</p>
+        <div id="main">
+
+
+            @include('layouts.navbar')
+            @yield('header')
+            @yield('content')
+        </div>
     </div>
+
+    @include('layouts.partials.footer')
+
+
+
 
     <script>
         ClassicEditor
