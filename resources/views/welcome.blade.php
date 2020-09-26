09a6816d0a655916c9a5bc42900e81ce68092c96
mzwae <mrmonjaie@gmail.com>
2020-09-26 22:15:33 +0100

    Add 3 latest articles carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,5 +10,44 @@
         </div>
     </div>
 </div>
-</div>
+<div id="carouselId" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselId" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselId" data-slide-to="1"></li>
+        <li data-target="#carouselId" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img src="/images/banner.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>First Article</h3>
+                <p>First excerpt</p>
+            </div>
+
+
+        </div>
+        @foreach($articles as $article)
+        <div class="carousel-item">
+            <img src="/images/banner.jpg" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h3>{{$article->title}}</h3>
+                <p>{{$article->excerpt}}</p>
+            </div>
+
+
+        </div>
+        @endforeach
+        </div>
+        <a class="carousel-control-prev" href="#carouselId" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselId" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+
+        </div>
+
+
 @endsection
