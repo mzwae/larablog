06be3cc29103f68602923640414520e9d17e1796
mzwae <mrmonjaie@gmail.com>
2020-09-26 22:14:50 +0100

    Attach the latest 3 articles to welcome view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Article;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,5 +25,12 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function welcome()
+    {
+        $articles = Article::latest()->take(3)->get();
+
+        return view('welcome', ['articles' => $articles]);
     }
 }
