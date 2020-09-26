849b6b8af5a0bd94faa196202c6b73fbbb7f8627
mzwae <mrmonjaie@gmail.com>
2020-09-26 22:13:36 +0100

    Update welcome page route

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 
 Auth::routes();
+
+Route::get('/', 'HomeController@welcome')->name('welcome');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
