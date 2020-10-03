91c336000081425ae38fce306002531cb4288a38
mzwae <mrmonjaie@gmail.com>
2020-10-03 09:17:05 +0100

    Update comment section design to improve ux

--- resources/views/layouts/partials/comment.blade.php
+++ resources/views/layouts/partials/comment.blade.php
@@ -1,5 +1,5 @@
 <div class="row d-flex justify-content-center mt-100 mb-100">
-    <div class="col-lg-6">
+    <div class="col-lg-12">
         @auth
         <form method="post" action="{{ route('comment.store', ['article' => $article->id]) }}" class="mb-3">
             @csrf
@@ -24,39 +24,29 @@
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
         @endauth
-        <div class="card">
-            <div class="card-body text-center">
-                @if(count($comments) > 0)
-                <h4 class="card-title">Latest Comments</h4>
-                @else
-                <h4 class="card-title">Be the first to comment &#128522;</h4>
-                @endif
+
+
+        @if(count($comments) > 0)
+            <h2 class="text-center font-weight-lighter m-5">Latest Comments</h2>
+            @else
+            <h2 class="text-center font-weight-lighter">Be the first to comment &#128522;</h2>
+        @endif
+
+        @foreach($comments as $comment)
+        <div class="media border p-3 m-5">
+            <img src="{{$user->find($comment->commented_id)->avatar}}?set=set2" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:60px;">
+            <div class="media-body">
+                <h4>{{ $user->find($comment->commented_id)->name }} <small> commented on <i>{{ $comment->created_at }}</i></small></h4>
+                <p>{{ $comment->comment }}</p>
+
+                @for($i = $comment->rate; $i > 0; $i--)
+                <span class="fa fa-star text-success"></span>
+                @endfor
+                @for($i = 5-$comment->rate; $i > 0; $i--)
+                <span class="fa fa-star"></span>
+                @endfor
             </div>
-            <div class="m-3">
-                <!-- Comment Row -->
-                @foreach ($comments as $comment)
-                <div class="d-flex">
-                    <div class="p-2">
-                        <img src="{{$user->find($comment->commented_id)->avatar}}?set=set2" alt="profile image" class="rounded-circle mr-2" width="50" height="50">
-                    </div>
-                    <div class="w-100">
-                        <h5>{{ $user->find($comment->commented_id)->name }} <small class="text-muted float-right">{{ $comment->created_at }}</small></h5>
-
-                        <p>{{ $comment->comment }}</p>
-
-                        @for($i = $comment->rate; $i > 0; $i--)
-                        <span class="fa fa-star text-success"></span>
-                        @endfor
-                        @for($i = 5-$comment->rate; $i > 0; $i--)
-                        <span class="fa fa-star"></span>
-                        @endfor
-
-                    </div>
-                </div>
-                <hr>
-                @endforeach
-
-            </div> <!-- Card -->
         </div>
+        @endforeach
     </div>
 </div>
